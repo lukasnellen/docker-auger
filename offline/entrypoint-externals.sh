bbc6ffa53f1078be90463d90e34c8e6d5b8cf2a2
Lukas Nellen <lukas@nucleares.unam.mx>
2018-08-07 12:23:49 -0500

    Added info how to run installed offline in container

--- offline/entrypoint-externals.sh
+++ offline/entrypoint-externals.sh
@@ -6,8 +6,10 @@ eval $(${APE} sh externals)
 mkdir /tmp/offline
 cd /tmp/offline
 export CM="cmake /auger/dist/offline -Dprefix=/auger/install/offline"
+echo 'eval $(/auger/install/offline/bin/auger-offline-config --env-sh)' >> OF.sh
 
 echo "Execute \$CM for default cmake configuration"
+echo "Execute '. OF.sh' to setup env from installed offline"
 
 if [ -n "${RHDEVTOOLSET}" ]; then
      exec scl enable ${RHDEVTOOLSET} /entrypoint-externals-stage2.sh "$@"
